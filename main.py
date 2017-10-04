dc9b534e97bd1fb6647c772d4c8ae21cf7de9fea
Nathan Hardy <nhardy@live.com.au>
2017-10-04 15:57:33 +1300

    Try something

--- main.py
+++ main.py
@@ -7,18 +7,34 @@ Author(s): Nathan Hardy
 
 import cProfile
 import functools
+import math
 import sys
 
-from typing import Generator
+from typing import Generator, Tuple
 
-def get_near_factors(number: int) -> Generator[int, None, None]:
+def get_nontrivial_near_factors(number: int) -> Generator[int, None, None]:
     """
     Generates all of the 'near factors' of a number
     """
 
-    for potential in range(1, number // 2 + 1):
-        if number // (number // potential) == potential:
-            yield potential
+    if number > 1:
+        g = math.floor(math.sqrt(number))
+
+        for potential in range(g + 1, number // 2 + 1):
+            if number // (number // potential) == potential:
+                yield potential
+
+COUNTS = {}
+
+def get_counts(number: int) -> Tuple[int, int]:
+    if number == 1:
+        return (1, 0)
+
+    green, red = get_counts(number - 1)
+    if get_colour(number) == 'G':
+        return (green + 1, red)
+    else:
+        return (green, red + 1)
 
 # Using a memoize cache for speedier repeated lookups
 @functools.lru_cache(maxsize=None)
@@ -30,12 +46,14 @@ def get_colour(number: int) -> str:
     if number == 1:
         return 'G'
 
+    green, red = get_counts(math.floor(math.sqrt(number)))
+
     counts = {
-        'G': 0,
-        'R': 0,
+        'G': green,
+        'R': red,
     }
 
-    for near_factor in get_near_factors(number):
+    for near_factor in get_nontrivial_near_factors(number):
         counts[get_colour(near_factor)] += 1
 
     return 'R' if counts['G'] > counts['R'] else 'G'
@@ -61,6 +79,9 @@ def main():
         # Add tuple(a, b) to the list of scenarios
         scenarios.append(tuple(map(int, unstripped_line.split())))
 
+    for i in range(max(map(lambda s: s[1], scenarios))):
+        get_counts(i)
+
     for (i, (a, b)) in enumerate(scenarios):
         if i == 0:
             print('\n')
@@ -72,7 +93,11 @@ def main():
 
 if __name__ == '__main__':
     for n in range(1, 20):
-        print(n, list(get_near_factors(n)))
+        print(n, list(get_nontrivial_near_factors(n)))
+
+    # print(get_counts(100))
+
+    # print(get_colour(10))
 
     cProfile.run('main()')
     # main()
